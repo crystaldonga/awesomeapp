de13e6dd94004f397f282a0bf3596ae4d349118f
crystaldonga <crystaldonga79@gmail.com>
2023-10-26 22:03:48 +0530

    mini project-2

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
-
 import React from 'react';
-import  ReactDOM   from 'react-dom';
-import  App  from './App';
+import ReactDOM  from 'react-dom';
+import Card from './Cards.jsx';
+ReactDOM.render(
+    <>
+    <Card imgsrc="http://cultartes.com/wp-content/uploads/2020/09/Dark-Season-3-German-series-is-streaming-online-on-Netflix-with-English-subtitles-release-date-27th-June-1-1024x597.jpg"
+        title="A Netflix Original Series"
+        sname="DARK"
+        link="https://www.netflix.com/in/title/80100172"
+    />
 
+<Card imgsrc="https://www.heavenofhorror.com/wp-content/uploads/2020/04/Extracurricular-netflix-review.jpg"
+        title="A Netflix Original Series"
+        sname="EXTRA CURRICULAR"
+        link="https://www.netflix.com/in/title/80990668"
+    />
 
-ReactDOM.render(<App/>,document.getElementById('root'));
+<Card imgsrc="http://wallpapercave.com/wp/wp1839579.jpg"
+        title="A Netflix Original Series"
+        sname="STRANGER THING"
+        link="https://www.netflix.com/in/title/80057281"
+    />
+    </>,document.getElementById('root')
+);
