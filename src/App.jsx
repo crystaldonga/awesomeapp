3913fe4af7cc08ee0225c284f48341851e3bdedf
crystaldonga <crystaldonga79@gmail.com>
2023-10-27 20:48:24 +0530

    27

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,7 @@ import React from 'react';
 import Amazon from './Amazon.jsx';
 import Netflix from './Netflix.jsx';
 let favs ="netflix";
-// const favs1=()=>{
-//     if(favs==="netflix"){
-//        return(<>
-//        <Amazon/>
-//        </>);
-//     }else{
-//       return(<>
-//        <Netflix/>
-//        </>);
-//     }
-// }
+
 const App=()=>{
 
 
