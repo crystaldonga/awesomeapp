e689e892e7bb27a62ccef4a5cf7bb8e214aa36ad
crystaldonga <crystaldonga79@gmail.com>
2023-10-27 16:34:23 +0530

    complete netfix mini project

--- src/index.js
+++ src/index.js
@@ -3,25 +3,22 @@ import ReactDOM  from 'react-dom';
 import Card from './Card.jsx';
 import Sdata from './Sdata.jsx';
 import './index.css';
+
+function ndata(val){
+    return (<>
+        <Card imgsrc={val.imgsrc}
+        title={val.title}
+        sname={val.sname}
+        link={val.link}
+    />
+    </>);
+}
+
 ReactDOM.render(
     <>
     <h1 className='d'>TOP 3 NETFLIX SERIES</h1>
-    <Card imgsrc={Sdata[0].imgsrc}
-        title={Sdata[0].title}
-        sname={Sdata[0].sname}
-        link={Sdata[0].link}
-    />
+    
 
- <Card imgsrc={Sdata[1].imgsrc}
-        title={Sdata[1].title}
-        sname={Sdata[1].sname}
-        link={Sdata[1].link}
-    />
-
- <Card imgsrc={Sdata[2].imgsrc}
-        title={Sdata[2].title}
-        sname={Sdata[2].sname}
-        link={Sdata[2].link}
-    />
+    {Sdata.map(ndata)};
     </>,document.getElementById('root')
 );
