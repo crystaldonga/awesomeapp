ab99ce8f3098ebcd7e792c0323d88be45186a70d
crystaldonga <crystaldonga79@gmail.com>
2023-11-01 00:20:23 +0530

    second part

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,68 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 
-const App=  ()=>{
-   const[name,fname] = useState("");
-   const[fullname,statename] = useState("");
-   const[lnameo,fnameo] = useState("");
-   const[lastname,statename1]=useState("");
+const App = () => {
+  const [fullName, setFullName] = useState({
+    fname: "",
+    lname: "",
+  });
+  // const[fullname,statename] = useState("");
+  // const[lnameo,fnameo] = useState("");
+  // const[lastname,statename1]=useState("");
 
-   const inputOne=(e)=>{
-      console.log(e.target.value);
-      fname(e.target.value);
-      
+  const inputEvent = (event) => {
+   console.log(event.target.value);
+   console.log(event.target.name);
 
-   }
+    const value = event.target.value;
+    const name = event.target.name;
 
-   const inputTwo=(e)=>{
-      console.log(e.target.value);
-      fnameo(e.target.value);
-      
+    setFullName((preValue) => {
+      //console.log(preValue);
+      if (name === "fname") {
+        return {
+          fname: value,
+          lname: preValue.lname,
+        };
+      } else if (name === "lname") {
+        return {
+          fname: preValue.fname,
+          lname: value,
+        };
+      }
+    });
+  };
 
-   }
-   const onSubmits=(e)=>{
-      e.preventDefault();
-      statename(name);
-      statename1(lnameo);
-      
-
-   }
-   return(<>
-   <form onSubmit={onSubmits}>
-      <h1>Hello {fullname} {lastname}</h1>
-      <input type="text" placeHolder="Enter your fname" onChange={inputOne} /><br/>
-      <input type="text" placeHolder="Enter your lname" onChange={inputTwo} />
-      <button type="submit">Click me</button>
-   </form>
-   </>)
-}
+  const onSubmits = (e) => {
+    e.preventDefault();
+    //alert("sumitted");
+    // statename(name);
+    // statename1(lnameo);
+  };
+  return (
+    <>
+      <form onSubmit={onSubmits}>
+        <h1>
+          Hello {fullName.fname} {fullName.lname}{" "}
+        </h1>
+        <input
+          type="text"
+          placeHolder="Enter your fname"
+          name="fname"
+          onChange={inputEvent}
+          value={fullName.fname}
+        />
+        <br />
+        <input
+          type="text"
+          placeHolder="Enter your lname"
+          name="lname"
+          onChange={inputEvent}
+          value={fullName.lname}
+        />
+        <button type="submit">Click me</button>
+      </form>
+    </>
+  );
+};
 
 export default App;
-
-
