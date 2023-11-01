a9a0b59de1b62c2ec39178020d749080ebac068a
crystaldonga <crystaldonga79@gmail.com>
2023-11-01 12:04:26 +0530

    40

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,108 @@
-import React,{useState} from 'react'
+import React, { useState } from "react";
 
-const App=  ()=>{
-   const[name,fname] = useState("");
-   const[fullname,statename] = useState("");
-   const[lnameo,fnameo] = useState("");
-   const[lastname,statename1]=useState("");
+const App = () => {
+  const [fullName, setFullName] = useState({
+    fname: "",
+    lname: "",
+    email: "",
+    phone: "",
+  });
+  // const[fullname,statename] = useState("");
+  // const[lnameo,fnameo] = useState("");
+  // const[lastname,statename1]=useState("");
 
-   const inputOne=(e)=>{
-      console.log(e.target.value);
-      fname(e.target.value);
-      
+  const inputEvent = (event) => {
+    console.log(event.target.value);
+    console.log(event.target.name);
 
-   }
+    // const value = event.target.value;
+    // const name = event.target.name;
+    const { value, name } = event.target;
 
-   const inputTwo=(e)=>{
-      console.log(e.target.value);
-      fnameo(e.target.value);
-      
+    setFullName((preValue) => {
+      //console.log(preValue);
+      if (name === "fname") {
+        return {
+          fname: value,
+          lname: preValue.lname,
+          email:preValue.email,
+          phone:preValue.phone
+        };
+      } else if (name === "lname") {
+        return {
+          fname: preValue.fname,
+          lname: value,
+          email:preValue.email,
+          phone:preValue.phone
+        };
+      }
+      else if (name === "email") {
+        return {
+          fname: preValue.fname,
+          lname:preValue.lname,
+          email:value,
+          phone:preValue.phone
+        };
+      }
+      else if (name === "phone") {
+        return {
+          fname: preValue.fname,
+          lname: preValue.lname,
+          email:preValue.email,
+          phone:value
+        };
+      }
+    });
+  };
 
-   }
-   const onSubmits=(e)=>{
-      e.preventDefault();
-      statename(name);
-      statename1(lnameo);
-      
+  const onSubmits = (e) => {
+    e.preventDefault();
+    alert("sumitted");
+    // statename(name);
+    // statename1(lnameo);
+  };
+  return (
+    <>
+      <form onSubmit={onSubmits}>
+        <h1>
+          Hello {fullName.fname} {fullName.lname}
+        </h1>
+        <p>{fullName.email}</p>
+        <p>{fullName.phone}</p>
+        <input
+          type="text"
+          placeHolder="Enter your fname"
+          name="fname"
+          onChange={inputEvent}
+          value={fullName.fname}
+        />
+        <br />
+        <input
+          type="text"
+          placeHolder="Enter your lname"
+          name="lname"
+          onChange={inputEvent}
+          value={fullName.lname}
+        />
 
-   }
-   return(<>
-   <form onSubmit={onSubmits}>
-      <h1>Hello {fullname} {lastname}</h1>
-      <input type="text" placeHolder="Enter your fname" onChange={inputOne} /><br/>
-      <input type="text" placeHolder="Enter your lname" onChange={inputTwo} />
-      <button type="submit">Click me</button>
-   </form>
-   </>)
-}
+        <input
+          type="text"
+          placeHolder="Enter your email"
+          name="email"
+          onChange={inputEvent}
+          value={fullName.email}
+        />
+        <input
+          type="text"
+          placeHolder="Enter your phone"
+          name="phone"
+          onChange={inputEvent}
+          value={fullName.phone}
+        />
+        <button type="submit">Click me</button>
+      </form>
+    </>
+  );
+};
 
 export default App;
-
-
