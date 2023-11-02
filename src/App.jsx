16d3a7eabac3a1123b21d265bafa597b634c7b9a
crystaldonga <crystaldonga79@gmail.com>
2023-11-02 21:23:06 +0530

    44

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,30 @@
 import React, { useState } from "react";
-import ToDoLists from "./ToDoLists.jsx";
+
 
 const App = () => {
-  const [inputList, setInputList] = useState();
-  const [Items, setItems] = useState([]);
-  const itemEvent = (event) => {
-    setInputList(event.target.value);
-  };
-  const listOfItem = () => {
-    setItems((oldItems) => {
-      return [...oldItems, inputList];
-    });
-    setInputList("");
-  };
-  const deletItem=(id)=>{
-    console.log("deleted");
-    setItems((oldItems)=>{
-   return oldItems.filter((arrElem ,index)=>{
-      return index!==id;
-   });
-    });
 
-  };
-  return (
-    <>
-      <div className="c">
-        <div className="a">
-          <h1 className="b">ToDo List</h1>
-          <br />
-          <input
-            type="text"
-            placeHolder="add a new item"
-            onChange={itemEvent}
-            value={inputList}
-          />
-          <button onClick={listOfItem}>+</button>
-          <ol type="none">
-            {/* <li>{inputList}</li> */}
-            {Items.map((itemVal, index) => {
-              return <ToDoLists text={itemVal} key={index} id={index} onSelect={deletItem} />;
-            })}
-          </ol>
-        </div>
-      </div>
-    </>
-  );
+  const[num,setnum] = useState(0);
+
+  const increment=()=>{
+    setnum(num+1);
+  }
+  const decrement=()=>{
+    if(num>0){
+      setnum(num-1);
+    }else{
+      alert("limit reached")
+    }
+  }
+   return(<>
+    <div className="main">
+    <div className="center">
+          <h1>{num}</h1>
+          
+          <button onClick={increment}>increm</button>
+          <button onClick={decrement}>decrem</button>
+    </div>
+
+    </div>
+   </>);
 };
 export default App;
