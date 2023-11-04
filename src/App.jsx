461a5e1fc9b35a5d19a653d1fe093356dd3de4fa
crystaldonga <crystaldonga79@gmail.com>
2023-11-04 10:59:39 +0530

    47

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,11 @@
 import React, { useState } from "react";
-import AddIcon from '@mui/icons-material/Add';
-import DeleteIcon from '@mui/icons-material/Delete';
-import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
+import Clock from 'react-digital-clock'
 
 
-const App = () => {
 
-  const[num,setnum] = useState(0);
-
-  const increment=()=>{
-    setnum(num+1);
-  }
-  const decrement=()=>{
-    if(num>0){
-      setnum(num-1);
-    }else{
-      alert("limit reached")
-    }
-  }
-   return(<>
-    <div className="main">
-    <div className="center">
-          <h1>{num}</h1>
-          <Tooltip title="add">
-          <Button className="btn1"
-          onClick={increment}>
-          <AddIcon/>
-          </Button>
-          </Tooltip>
-          <Tooltip title="Delete">
-          <Button className="btn2"
-          onClick={decrement}>
-          <DeleteIcon/>
-          </Button>
-          </Tooltip>
-    </div>
-
-
-    </div>
-   </>);
-};
+const App=()=>{
+  return(<>
+   <Clock/>
+  </>)
+}
 export default App;
