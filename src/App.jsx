75034c323011f0cd85d7137c1af19ae6a3888026
crystaldonga <crystaldonga79@gmail.com>
2023-11-04 12:18:13 +0530

    48

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
 import React, { useState } from "react";
-import Clock from 'react-digital-clock'
+import Button from '@mui/material/Button';
+import AddIcon from '@mui/icons-material/Add';
+import ListCom from './ListCom';
 
 
 
 const App=()=>{
+  const[item,setItem] = useState();
+  const[newItem,setNewItem] = useState([]);
+  const itemEvent=(event)=>{
+     setItem(event.target.value);
+  }
+  const listOfItem=()=>{
+       setNewItem((prevValue)=>{
+           return [...prevValue,item]
+       })
+       setItem("");
+  }
   return(<>
-   <Clock/>
+   <div className="main">
+    <div className="center">
+     <h1> ToDo List</h1>
+     <br/>
+     <input type="text" placeholder="add a new Item" onChange={itemEvent} value={item}/>
+     <Button className="newbtn" onClick={listOfItem}><AddIcon/></Button>
+     <br/>
+     <ol type="none">
+     
+     { newItem.map((val,index)=>{
+     return <ListCom text={val} key={index}/>
+      })}
+     </ol>
+
+    </div>
+
+   </div>
   </>)
 }
 export default App;
