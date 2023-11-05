fabe127012aaad6ed5ef7e055f24d1f594ff77da
crystaldonga <crystaldonga79@gmail.com>
2023-11-05 12:38:31 +0530

    58

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
 import React,{useState,useEffect}from 'react'
 const App=()=>{
   const[num,setNum] =useState(0);
-  const[nums,setNums] =useState(0);
+  
   useEffect(()=>{
-    alert("cliked me")
-  },[num])
+    document.title = `you clicked me ${num}`
+  })
   return(<>
     <button onClick={()=>{
       setNum(num+1)
     }}>Click Me {num}
     </button>
 
-    <button onClick={()=>{
-      setNums(nums+1)
-    }}>Click Me {nums}
-    </button>
+    
   </>)
 }
 export default App;
