aa1ea9d49810781d5c10d92bbc1e059b5b2f9331
crystaldonga <crystaldonga79@gmail.com>
2023-11-06 15:28:07 +0530

    71

--- src/Error.jsx
+++ src/Error.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import {NavLink} from "react-router-dom";
 const Error=()=>{
     return(<>
-        <h1>oops ! page not found</h1>
+        <h1>404 error page</h1>
+        <NavLink to="/">go back</NavLink>
     </>)
 }
 export default Error;
