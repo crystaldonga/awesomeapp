536f80e8ceba9671ec6130eb84e0cc78511ee834
crystaldonga <crystaldonga79@gmail.com>
2023-11-06 13:38:05 +0530

    69

--- src/User.jsx
+++ src/User.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
-import {useParams,useLocation} from 'react-router-dom'
+import {useParams,useLocation,useHistory} from 'react-router-dom'
 const User=()=>{
     const{fname,lname} = useParams();
     const location = useLocation();
+    const history = useHistory();
     console.log(location);
     return(<>
         <h1>my {fname} {lname} </h1>
         <br/>
         <h2>my current location is {location.pathname}</h2>
-        {location.pathname===`/user/vinod/thapa`? (<button onClick={()=>alert("clicked me")}></button>):null}
+        {location.pathname===`/user/vinod/thapa`? (<button onClick={()=>{history.goBack()
+        history.push('/')
+        }}> 
+        goBack
+        </button>):null}
     </>)
 }
 export default User;
