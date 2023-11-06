b3d65813ae53118edcfee5467adbc1a6d6be0a4c
crystaldonga <crystaldonga79@gmail.com>
2023-11-06 13:05:48 +0530

    68

--- src/User.jsx
+++ src/User.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-import {useParams} from 'react-router-dom'
+import {useParams,useLocation} from 'react-router-dom'
 const User=()=>{
     const{fname,lname} = useParams();
+    const location = useLocation();
+    console.log(location);
     return(<>
         <h1>my {fname} {lname} </h1>
+        <br/>
+        <h2>my current location is {location.pathname}</h2>
+        {location.pathname===`/user/vinod/thapa`? (<button onClick={()=>alert("clicked me")}></button>):null}
     </>)
 }
 export default User;
