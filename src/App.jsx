c21429b029045bbb9d8b65ab77cd0d1bc102fbf5
crystaldonga <crystaldonga79@gmail.com>
2023-11-07 11:30:16 +0530

    73

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,11 @@
 import React from 'react'
-import {Switch,Route,Redirect} from "react-router-dom"
-import About from './About.jsx'
-import Contact from './Contact.jsx'
-import Error from './Error.jsx'
-import Menu from './Menu.jsx'
-import Name from './Name'
-import Service from './Service'
-import User from './User';
-import Search from './Search'
+import "../node_modules/bootstarp/dist/css/bootstrap.min.css";
 
 const App=()=>{
 
      
   return(<>
-    <Menu/>
-    <Switch>
-      <Route exact path="/" component={()=>{
-        <About/>
-      }}/>
-      <Route exact path="/Contact" render={()=>{
-        <Contact name ="contact"/>
-      }}/>
-      <Route exact path="/Service" component={Service}/>
-      <Route exact path="/User/:fname/:lname" component={User}/>
-      <Route path="/User/Name" component={Name}/>
-      <Route path="/Search" component={Search}/>
-      {/* <Route  component={Error}/> */}
-      <Redirect to="/" />
-
-      
-      
-    </Switch>
+   <h1 className="text-center">welcome</h1>
   </>);
 }
 
