1a26fdf6223723c901c00e456816212837c66dac
crystaldonga <crystaldonga79@gmail.com>
2023-11-07 23:00:18 +0530

    71

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,27 @@
-import React from 'react'
-import {Switch,Route} from "react-router-dom"
-import About from './About.jsx'
-import Contact from './Contact.jsx'
-import Error from './Error.jsx'
-import Menu from './Menu.jsx'
-import Name from './Name'
-import Service from './Service'
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import About from './About.jsx';
+import Contact from './Contact.jsx';
+import Error from './Error.jsx';
+import Menu from './Menu.jsx';
+import Name from './Name';
+import Service from './Service';
 import User from './User';
+import Home from './Home';
 
-const App=()=>{
-
-     
-  return(<>
-    <Menu/>
-    <Switch>
-      <Route exact path="/" component={()=>{
-        <About/>
-      }}/>
-      <Route exact path="/Contact" render={()=>{
-        <Contact name ="contact"/>
-      }}/>
-      <Route exact path="/Service" component={Service}/>
-      <Route exact path="/User/:fname/:lname" component={User}/>
-      <Route path="/User/Name" component={Name}/>
-      <Route  component={Error}/>
-
-      
-      
-    </Switch>
-  </>);
-}
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/user/:fname/:lname" element={<User />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/contact" element={<Contact name="contact" />} />
+      <Route path="/service" element={<Service />} />
+      <Route path="/menu" element={<Menu />} />
+      <Route path="/name" element={<Name />} />
+      <Route path="*" element={<Error />} />
+    </Routes>
+  );
+};
 
 export default App;
--- src/User.jsx
+++ src/User.jsx
@@ -1,19 +1,19 @@
 import React from 'react'
-import {useParams,useLocation,useHistory} from 'react-router-dom'
+import {useParams,useLocation,useNavigate} from 'react-router-dom'
 const User=()=>{
     const{fname,lname} = useParams();
     const location = useLocation();
-    const history = useHistory();
+    const history = useNavigate();
     console.log(location);
     return(<>
         <h1>my {fname} {lname} </h1>
         <br/>
         <h2>my current location is {location.pathname}</h2>
-        {location.pathname===`/user/vinod/thapa`? (<button onClick={()=>{history.goBack()
+        {/* {location.pathname===`/user/vinod/thapa`? (<button onClick={()=>{history.goBack()
         history.push('/')
         }}> 
         goBack
-        </button>):null}
+        </button>):null} */}
     </>)
 }
 export default User;
